6f067d52b12f9f295f97b6a181f4e1e1c3355bd5
Synonymous64 <prajwalurkude007@gmail.com>
2023-08-16 18:56:10 +0530

    initial setup and navbar created

---
+++ expressive-ai/components/navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Menu, Sparkles } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Poppins } from "next/font/google";
+import { UserButton } from "@clerk/nextjs";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            Expressive.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-purple-500 text-purple-600 ml-2" />
+        </Button>
+        <ModeToggle />
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
